44c3085bca3475e0b1f86ccd19714dd39be377a2
davhsi <davish.std@gmail.com>
2025-05-19 16:04:42 +0530

    backend url env

--- backend/config/cron.js
+++ backend/config/cron.js
@@ -1,25 +1,36 @@
 const { CronJob } = require('cron');
 const https = require('https');
 
-const backendUrl = 'https://freshfleet.onrender.com'; // Replace with a specific endpoint if necessary
+require('dotenv').config();
 
-const job = new CronJob('*/14 * * * *', function () {
-  console.log(`Attempting to keep server active`);
+const backendUrl = process.env.BACKEND_URL;
+if (!backendUrl) {
+  console.error('Error: BACKEND_URL is not defined in environment variables.');
+  process.exit(1);
+}
 
-  https
-    .get(backendUrl, (res) => {
-      if (res.statusCode === 200) {
-        console.log('Server is active');
-      } else {
-        console.error(
-          `Failed to reach server, status code: ${res.statusCode}`
-        );
-      }
-    })
-    .on('error', (err) => {
-      console.error('Error during server ping:', err.message);
-    });
-}, null, true, 'America/Los_Angeles');
+const job = new CronJob(
+  '*/14 * * * *',
+  function () {
+    console.log(`Attempting to keep server active at ${backendUrl}`);
 
-// Export the cron job.
+    https
+      .get(backendUrl, (res) => {
+        if (res.statusCode === 200) {
+          console.log('Server is active');
+        } else {
+          console.error(
+            `Failed to reach server, status code: ${res.statusCode}`
+          );
+        }
+      })
+      .on('error', (err) => {
+        console.error('Error during server ping:', err.message);
+      });
+  },
+  null,
+  true,
+  'America/Los_Angeles'
+);
+
 module.exports = job;
